53e4024133a1efa13519c9e9ed4001d3ab59037a
Tomas cabrera <tomascabrera918@gmail.com>
2024-11-13 17:56:04 -0300

    Changes to Structure

---
+++ src/com/gamehub/gui/LibraryGUI.java
@@ -0,0 +1,45 @@
+/*
+ * Created by JFormDesigner on Wed Nov 13 17:52:52 ART 2024
+ */
+
+package com.gamehub.gui;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author Administrator
+ */
+public class LibraryGUI extends JPanel {
+    public LibraryGUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+
+        //======== this ========
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
+        0, 0, 0, 0) , "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
+        . BOTTOM, new java .awt .Font ("D\u0069al\u006fg" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
+        red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
+        beans .PropertyChangeEvent e) {if ("\u0062or\u0064er" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
---
+++ src/com/gamehub/gui/ProfileGUI.java
@@ -0,0 +1,47 @@
+/*
+ * Created by JFormDesigner on Wed Nov 13 17:52:32 ART 2024
+ */
+
+package com.gamehub.gui;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author Administrator
+ */
+public class ProfileGUI extends JPanel {
+    public ProfileGUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+
+        //======== this ========
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax .
+        swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing .border
+        . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dialo\u0067"
+        , java .awt . Font. BOLD ,12 ) ,java . awt. Color .red ) , getBorder
+        () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java
+        . beans. PropertyChangeEvent e) { if( "borde\u0072" .equals ( e. getPropertyName () ) )throw new RuntimeException
+        ( ) ;} } );
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
