aadb33d3a0601f411f8a98d21436df40648659ed
joacoloool <joaquinalbornoz555@gmail.com>
2024-11-14 14:37:32 -0300

    Revert "Merge branch 'UI-IMPLEMENTATION' into intent"
    This reverts commit 0abb0bd2e439c09c0fb00f827764a35924a90b10, reversing
    changes made to cbbcf2d9e5e782887dd813b79edef9dde9bd54d2.

--- src/com/gamehub/gui/ProfileGUI.java
+++ src/com/gamehub/gui/ProfileGUI.java
@@ -1,5 +1,5 @@
 /*
- * Created by JFormDesigner on Wed Nov 13 17:52:32 ART 2024
+ * Created by JFormDesigner on Thu Nov 14 11:42:10 ART 2024
  */
 
 package com.gamehub.gui;
@@ -17,41 +17,41 @@ public class ProfileGUI extends JPanel {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+        // Generated using JFormDesigner Evaluation license - Joaquin Albornoz
         label1 = new JLabel();
 
         //======== this ========
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.
-        border.EmptyBorder(0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing.border.TitledBorder.CENTER
-        ,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("D\u0069al\u006fg",java.awt.Font
-        .BOLD,12),java.awt.Color.red), getBorder())); addPropertyChangeListener(
-        new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062or\u0064er"
-        .equals(e.getPropertyName()))throw new RuntimeException();}});
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+        EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border. TitledBorder. CENTER, javax. swing
+        . border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ),
+        java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+        { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName () ))
+        throw new RuntimeException( ); }} );
 
         //---- label1 ----
-        label1.setText("perfil");
+        label1.setText("Pene Profile");
 
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(182, 182, 182)
+                    .addGap(216, 216, 216)
                     .addComponent(label1)
-                    .addContainerGap(313, Short.MAX_VALUE))
+                    .addContainerGap(643, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(123, 123, 123)
+                    .addGap(182, 182, 182)
                     .addComponent(label1)
-                    .addContainerGap(225, Short.MAX_VALUE))
+                    .addContainerGap(346, Short.MAX_VALUE))
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - VALERIA MARQUEZ
+    // Generated using JFormDesigner Evaluation license - Joaquin Albornoz
     private JLabel label1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
