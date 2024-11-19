3b4d027e8c20aa70ef46883e0977d242d14493af
joacoloool <joaquinalbornoz555@gmail.com>
2024-11-19 03:06:51 -0300

    comment steamhelper

--- src/com/gamehub/utils/SteamHelper.java
+++ src/com/gamehub/utils/SteamHelper.java
@@ -1,46 +1,66 @@
 package com.gamehub.utils;
-import com.gamehub.enums.Genre;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
+import com.gamehub.enums.Genre; // Enum para los géneros de los juegos
+import org.jsoup.Jsoup; // Biblioteca para realizar conexiones HTTP y analizar HTML
+import org.jsoup.nodes.Document; // Representa un documento HTML
+import org.jsoup.nodes.Element; // Representa un elemento HTML
+import java.io.IOException; // Excepción para manejar errores de IO
+import java.util.ArrayList; // Implementación de List para almacenar elementos
+import java.util.List; // Interfaz para colecciones ordenadas
 
 public class SteamHelper {
 
-    private static final String apiKey = System.getenv("STEAM_API_KEY"); // No share!
+    // Clave de API para autenticar las solicitudes a la API de Steam, obtenida de las variables de entorno
+    private static final String apiKey = System.getenv("STEAM_API_KEY"); // No compartir!
 
-
+    /**
+     * Busca el AppID de un juego en Steam basado en su título.
+     *
+     * @param title Título del juego a buscar.
+     * @return Devuelve el AppID como una cadena. Retorna una cadena vacía si no se encuentra.
+     */
     public static String getAppid(String title) {
         try {
+            // Reemplaza espacios en el título por '+'
             String titleF = title.replace(" ", "+");
+            // Construye la URL de búsqueda
             String url = "https://store.steampowered.com/search/?term=" + titleF;
+            // Realiza la conexión y obtiene el documento HTML
             Document doc = Jsoup.connect(url).get();
+            // Obtiene el primer resultado de búsqueda
             Element firstResult = doc.select(".search_result_row").first();
 
-            // Verifica si firstResult no es nulo
+            // Verifica si se encontró un resultado
             if (firstResult != null && !titleF.isEmpty()) {
                 String foundTitle = firstResult.select(".title").text();
+                // Compara las letras del título encontrado con el título proporcionado
                 if (compareLetters(foundTitle, title)) {
-                    return firstResult.attr("data-ds-appid");
+                    return firstResult.attr("data-ds-appid"); // Retorna el AppID
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); // Lanza excepción en caso de error de conexión
         }
-        return ""; // Retorna una cadena vacía si no se encuentra el appid
+        return ""; // Retorna una cadena vacía si no se encuentra el AppID
     }
 
+    /**
+     * Obtiene información específica de un juego utilizando su AppID.
+     *
+     * @param appId ID de la aplicación del juego.
+     * @param infoType Tipo de información solicitada (ej. "description", "name", "image", etc.).
+     * @return La información solicitada como una cadena. Retorna un mensaje de error si el tipo no es soportado.
+     */
     public static String getGameInfo(String appId, String infoType) {
         if (!appId.isEmpty()) {
             try {
-
+                // Construye la URL para obtener detalles del juego
                 String url = "https://store.steampowered.com/api/appdetails?appids=" + appId + "&key=" + apiKey;
+                // Realiza la conexión y obtiene la respuesta en formato JSON
                 Document doc = Jsoup.connect(url).ignoreContentType(true).get();
                 String jsonResponse = doc.text();
 
-
+                // Analiza la respuesta JSON según el tipo de información solicitada
                 switch (infoType.toLowerCase()) {
                     case "description":
                         return jsonResponse
@@ -63,7 +83,6 @@ public class SteamHelper {
                                 .replaceAll("\"", "")
                                 .split("header_image:")[1].split(",")[0].trim().replace("/", "").replace("\\", "/");
 
-
                     case "release":
                         return jsonResponse
                                 .replaceAll("\"", "")
@@ -71,46 +90,56 @@ public class SteamHelper {
                                 .split("}")[0];
 
                     case "genre":
-                        String genreData = jsonResponse.replaceAll("\"", "").split("genres:")[1].split("]")[0].trim();
-                        List<String> foundGenres = new ArrayList<>();
-
-                        String[] genresArray = genreData.split("},\\{");
-
-                        for (String genre : genresArray) {
-                            String description = genre.split("description:")[1].split(",")[0].trim();
-
-                            for (Genre g : Genre.values()) {
-                                if (description.equalsIgnoreCase(g.name())) {
-                                    foundGenres.add(g.name());
-                                }
-                            }
-                        }
-
-                        return String.join(", ", foundGenres);
+                        List<String> foundGenres = getStrings(jsonResponse);
+                        return String.join(", ", foundGenres); // Retorna los géneros como una cadena
 
                     default:
-                        return "Tipo de información no soportada: " + infoType;
+                        return "Tipo de información no soportada: " + infoType; // Mensaje de error para tipos no soportados
                 }
             } catch (Exception e) {
-                System.out.println("No fue posible conectar a la base de datos de steam.");
+                System.out.println("No fue posible conectar a la base de datos de steam."); // Mensaje de error en caso de fallo
             }
         }
-        return null;
+        return null; // Retorna null si el AppID está vacío
     }
 
+    private static List<String> getStrings(String jsonResponse) {
+        String genreData = jsonResponse.replaceAll("\"", "").split("genres:")[1].split("]")[0].trim();
+        List<String> foundGenres = new ArrayList<>();
+        // Analiza los géneros del juego
+        String[] genresArray = genreData.split("},\\{");
+        for (String genre : genresArray) {
+            String description = genre.split("description:")[1].split(",")[0].trim();
 
+            // Compara con los géneros definidos en el enum
+            for (Genre g : Genre.values()) {
+                if (description.equalsIgnoreCase (g.name())) {
+                    foundGenres.add(g.name()); // Agrega el género encontrado a la lista
+                }
+            }
+        }
+        return foundGenres;
+    }
+
+    /**
+     * Compara las dos primeras letras de dos cadenas para verificar si son iguales.
+     *
+     * @param str1 Primera cadena a comparar.
+     * @param str2 Segunda cadena a comparar.
+     * @return true si las dos primeras letras son iguales; false en caso contrario.
+     */
     private static boolean compareLetters(String str1, String str2) {
         boolean flag = false;
 
+        // Verifica si alguna de las cadenas está vacía
         if (str1.isEmpty() || str2.isEmpty()) {
             return flag;
         }
+        // Compara las dos primeras letras de ambas cadenas
         if (Character.toLowerCase(str1.charAt(0)) == Character.toLowerCase(str2.charAt(0)) &&
                 Character.toLowerCase(str1.charAt(1)) == Character.toLowerCase(str2.charAt(1))) {
             flag = true; // Las dos primeras letras son iguales
         }
-        return flag;
+        return flag; // Retorna el resultado de la comparación
     }
-
 }
-
